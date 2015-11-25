bff63a05c9e11302bba11d692c05e1c789daadcb
Asher Foster <asher@developster.io>
2015-11-26 11:29:58 +1300

    Working on support for GH Pages

--- js/photos.js
+++ js/photos.js
@@ -1,10 +1,10 @@
-var id = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
+var id = window.location.href.substring(window.location.href.lastIndexOf("?") + 1);
 var app = angular.module('afPhotos', []);
 app.controller('ListController', function($scope){
     $scope.photos = photos;
 });
 app.controller("PhotoController", function($scope){
-    $scope.photo = null;
+    $scope.id = id;
     if(photos[id]){
         $scope.photo = photos[id];
         $scope.photoExists = true
