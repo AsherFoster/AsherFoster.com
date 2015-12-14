b1198f32cc2f1b7098f666204e49afb51a04968f
Asher Foster <asher@developster.io>
2015-12-14 17:26:18 +1300

    Added theme switcher to home page, soon to be rolled out to all pages

--- js/home.js
+++ js/home.js
@@ -1,4 +1,7 @@
 var app = angular.module('afHome', []);
 app.controller('postController', function($scope){
     $scope.posts = posts;
-});
+})
+.controller('navController', function($scope){
+    
+})
