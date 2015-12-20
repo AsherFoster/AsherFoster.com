99c2e7709f07244d39f8da859458125e5da4dcfb
Asher Foster <me@asherfoster.com>
2015-12-20 19:27:02 +1300

    Fixed theme switcher

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 var app = angular.module('site', []), theme;
-app.controller('navController', function($scope, $rootScope){
-    theme = $scope.theme ? 'dark' : 'light';
+function switchTheme(to){
+    document.getElementsByTagName("body")[0].setAttribute("theme", to ? 'dark' : 'light')
+}
+app.controller('navController', function($scope, $interval){
+//    theme = $scope.theme ? 'dark' : 'light';
+    $interval(function(){switchTheme($scope.theme)}, 10);
 })
