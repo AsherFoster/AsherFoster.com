2c02ffca3ddfb81aeea2e655be6b28c2d33d9575
Asher Foster <me@asherfoster.com>
2015-12-30 23:09:45 +1300

    Im sleeping now

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-var app = angular.module('site', []), theme, nav = {
+var app = angular.module('site', []), theme, menu, main, nav = {
     expand: function(){
-        document.getElementsByTagName("nav")[0].style.left = 0;
-        document.getElementsByTagName("main")[0].style.left = "";
-        document.getElementsByTagName("main")[0].style.width = "";
+        menu.style.left = 0;
+        main.style.left = "";
+        main.style.width = "";
     },
     collapse: function(){
-        document.getElementsByTagName("nav")[0].style.left = "-25%";
-        document.getElementsByTagName("main")[0].style.left = "65px";
-        document.getElementsByTagName("main")[0].style.width = "85%";
+        menu.style.left = "-25%";
+        main.style.left = "65px";
+        main.style.width = "85%";
     }
 };
 app.controller('navController', function($scope, $interval){
     $interval(function(){
         document.getElementsByTagName("body")[0].setAttribute("theme", $scope.theme ? 'dark' : 'light')
     }, 10);
-})
+});
+window.onload = function(){
+    menu = document.getElementsByTagName("nav")[0],
+    main = document.getElementsByTagName("main")[0]
+}
