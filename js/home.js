bba02c3e5cd102bbc6483645f2e5b3f3451fabf1
Asher Foster <me@asherfoster.com>
2016-01-10 22:54:27 +1300

    Quite a big new update
    Overhauled navigation, made the home page better(?) and loads more

--- js/home.js
+++ js/home.js
@@ -1,3 +1,10 @@
 app.controller('postController', function($scope){
     $scope.posts = posts;
-})
+});
+d.onscroll = function(){
+    if(pageYOffset > document.body.clientHeight * 0.7){
+        d.getElementsByClassName("nav")[0].style.background = "#555"
+    }else{
+        d.getElementsByClassName("nav")[0].style.background = ""
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,21 +1,2 @@
-var app = angular.module('site', []), theme, menu, main, nav = {
-    expand: function(){
-        menu.style.left = 0;
-        main.style.left = "";
-        main.style.width = "";
-    },
-    collapse: function(){
-        menu.style.left = "-25%";
-        main.style.left = "65px";
-        main.style.width = "85%";
-    }
-};
-app.controller('navController', function($scope, $interval){
-    $interval(function(){
-        document.getElementsByTagName("body")[0].setAttribute("theme", $scope.theme ? 'dark' : 'light')
-    }, 10);
-});
-window.onload = function(){
-    menu = document.getElementsByTagName("nav")[0],
-    main = document.getElementsByTagName("main")[0]
-}
+var app = angular.module('site', []),
+    d = document
